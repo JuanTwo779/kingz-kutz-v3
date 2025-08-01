07385c8b9714f5a58938a24f9335771af02c6982
juanm <juanmangubat@gmail.com>
2025-08-01 18:01:55 +1000

    Header and router adjustments

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import { BsScissors } from 'react-icons/bs'
 
 import images from '../../constants/images';
 
+import { Link } from 'react-router-dom';
+
 import './Navbar.css';
 
 const Navbar = () => {
@@ -14,10 +16,9 @@ const Navbar = () => {
       <div className="rounded-background"></div>
 
       <ul className="app__navbar-links items-left">
-        <li><a className='p__opensans' href="#barber">About</a></li>
-        <li><a className='p__opensans' href="#services">Services</a></li>
-        <li><a className='p__opensans' href="#rules">Rules</a></li>
-        <li><a className='p__opensans' href="#intro">Video</a></li>
+        <li><Link to="/" className='p__opensans'>Home</Link></li>
+        <li><Link to="/about" className='p__opensans'>About</Link></li>
+        <li><Link to="/services" className='p__opensans'>Services</Link></li>
       </ul>
 
 
@@ -30,10 +31,9 @@ const Navbar = () => {
       </div>
 
       <ul className='app__navbar-links items-right'>
-        <li><a className='p__opensans' href="#photos" >Gallery</a></li>
-        <li><a className='p__opensans' href="#assistant" >Assist</a></li>
-        <li><a className='p__opensans' href="#contact">Contact</a></li>
-        <li><a className='p__opensans' href="#booking">Book</a></li>
+        <li><Link to="/gallery" className='p__opensans'>Gallery</Link></li>
+        <li><Link to="/booking" className='p__opensans'>Booking</Link></li>
+        <li><Link to="/contact" className='p__opensans'>Contact</Link></li>
       </ul>
 
       <div className="app__navbar-smallscreen">
@@ -43,14 +43,12 @@ const Navbar = () => {
         <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
           <BsScissors fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
           <ul className='app__navbar-smallscreen_links' onClick={() => setToggleMenu(false)}>
-            <li><a href="#barber">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#rules">Rules</a></li>
-            <li><a href="#intro">Video</a></li>
-            <li><a href="#photos">Gallery</a></li>
-            <li><a href="#assistant">Assist</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#booking">Book</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/booking">Booking</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
         )}
