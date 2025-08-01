0bc993a1fd1dc530ae690309bde7aa2f3335613c
juanm <juanmangubat@gmail.com>
2025-08-01 18:08:40 +1000

    React router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { Navbar } from './components';
+import { Navbar} from './components';
+import { Footer } from './container'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
