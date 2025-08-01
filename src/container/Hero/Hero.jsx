b6e5f4acdc99d0d5764e355db144f8f6eee8651e
juanm <juanmangubat@gmail.com>
2025-08-01 23:28:37 +1000

    Call to action hero page

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 import { GiQueenCrown } from "react-icons/gi";
+import Intro from "../Intro/Intro";
 
 import './Hero.css';
+import Rules from "../Rules/Rules";
 
 const Hero = () => (
-     <div className='app__hero-2' id='hero'>
-          <div className='app__hero-info'>
-               <GiQueenCrown />
-               <h1 className='app__hero-h1'>Serving royalty</h1>
-               <h2 className='p__cormorant app__hero-h2'>One cut at a time</h2>
-            
+     <>
+          <div className='app__hero-2' id='hero'>
+               <div className='app__hero-info'>
+                    <GiQueenCrown />
+                    <h1 className='app__hero-h1'>Serving royalty</h1>
+                    <h2 className='app__hero-h2'><Link to='/booking' className="p__cormorant custom__button">Book Now</Link></h2>
+               </div>
           </div>
-     </div>
+     </>
 );
 
 export default Hero;
