f1788e35b23c9dfc25f18ca240aadc421db6549d
JuanTwo779 <juanmangubat@gmail.com>
2025-08-14 23:47:21 +1000

    Gallery page: gallery specific carousel css + SEO alt text for images

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
@@ -13,17 +11,17 @@ const Gallery = () => {
       <div className="app__wrapper_info">
         <SubHeading title="Follow us on Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ margin: '0.5rem 0 2rem 0' }}>Indulge your senses in a visual feast of impeccable styles. Browse our exquisite haircut gallery for a glimpse of possibilities. Let your imagination cook and discover your perfect royal look.</p>
+        <p className="p__opensans" style={{ margin: '0.5rem 0 2rem 0' }}>Explore our gallery of expertly crafted styles. Each cut showcases precision, creativity, and attention to detail, offering inspiration for your next look. Discover the style that best reflects your individuality.</p>
         <a href="https://www.instagram.com/blurryvision.kk/" target='_blank' rel="noreferrer" type="button" className="custom__button" style={{ textDecoration: 'none' }}>@blurryvision.kk</a>
       </div>
       
       <div className="app__wrapper_img app__wrapper_img">
-          <Carousel className='app__carousel'>
+          <Carousel className='gallery__carousel'>
             {[images.gallery1, images.gallery2, images.gallery3, images.gallery4, images.gallery5, 
             images.gallery6, images.gallery7, images.gallery8, images.gallery9, images.gallery10, 
             images.gallery11, images.gallery12].map((image) => (
               <Carousel.Item>
-                <img src={image} alt='Men mens fade haircut trim' className='d-block w-100 app_carousel-img' />
+                <img src={image} alt='Men mens fade haircut trim undercut textured fringe' className='d-block w-100 gallery_carousel-img' />
               </Carousel.Item>
             ))}
           </Carousel>
